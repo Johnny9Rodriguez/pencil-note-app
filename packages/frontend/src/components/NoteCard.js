420e068d06821a7b5f11896b8346c0c41e3ebcf7
Joe Pytlik <mail@joepytlik.de>
2023-12-14 22:07:41 +0100

    Fixed note flashing when deleting a note.

--- packages/frontend/src/components/NoteCard.js
+++ packages/frontend/src/components/NoteCard.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import { Icon } from '@iconify/react';
-import { useState } from 'react';
 import { useDispatch } from 'react-redux'
 import { remove, select } from '../slices/noteDataSlice'
 
-export const NoteCard = ({ id, title, content, selected }) => {
-  const [elementMouseOver, setElementMouseOver] = useState(false);
-  const [deleteMouseOver, setDeleteMouseOver] = useState(false);
+export const NoteCard = ({ id, title, content, selected, isHovered, isHoveredDelete, elementIndex, setHoverIndex, setHoverDelete }) => {
   const dispatch = useDispatch();
 
-  let noteCardStyle;
-  if (elementMouseOver) {
+  let noteCardStyle = 'text-white bg-darkTeal bg-opacity-50';
+  if (isHovered) {
     noteCardStyle = 'text-black bg-brightTeal bg-opacity-100'
   } else if (selected) {
     noteCardStyle = 'text-white bg-darkTeal bg-opacity-100 border border-brightTeal'
-  } else {
-    noteCardStyle = 'text-white bg-darkTeal bg-opacity-50'
   }
 
   const handleDelete = async (e) => {
@@ -36,7 +31,6 @@ export const NoteCard = ({ id, title, content, selected }) => {
 
     if (response.status === 202) {
       dispatch(remove(id));
-      console.log('Note deleted successfully.');
     } else {
       console.log('Internal server error.');
     }
@@ -45,23 +39,23 @@ export const NoteCard = ({ id, title, content, selected }) => {
   return (
     <div
       className={'relative w-full'}
-      onMouseEnter={() => setElementMouseOver(true)}
-      onMouseLeave={() => setElementMouseOver(false)}
+      onMouseEnter={() => setHoverIndex(elementIndex)}
+      onMouseLeave={() => setHoverIndex(null)}
     >
       <button
-        className={`${elementMouseOver ? '' : 'hidden'} absolute z-10 right-1 top-1/4 text-3xl ${deleteMouseOver ? 'text-brightCrimson' : 'text-white'}`}
-        onMouseEnter={() => setDeleteMouseOver(true)}
-        onMouseLeave={() => setDeleteMouseOver(false)}
+        className={`${isHovered ? '' : 'hidden'} absolute z-10 right-1 top-1/4 text-3xl ${isHoveredDelete ? 'text-brightCrimson' : 'text-white'}`}
+        onMouseEnter={() => setHoverDelete(elementIndex)}
+        onMouseLeave={() => setHoverDelete(null)}
         onClick={handleDelete}
       >
-        {deleteMouseOver ? (<Icon icon="material-symbols:delete-sharp" />) : (<Icon icon="material-symbols-light:delete-outline-sharp" />)}
+        {isHoveredDelete ? (<Icon icon="material-symbols:delete-sharp" />) : (<Icon icon="material-symbols-light:delete-outline-sharp" />)}
       </button>
       <button
         className={`flex flex-col w-full p-2 ${noteCardStyle}`}
         onClick={() => { dispatch(select({ id: id, title: title, content: content })) }}
       >
-        <p className={`${elementMouseOver ? 'w-5/6' : 'w-full'} note-card font-bold`}>{title === '' ? 'New Note' : title}</p>
-        <p className={`${elementMouseOver ? 'w-5/6' : 'w-full'} note-card`}>{content === '' ? '...' : content}</p>
+        <p className={`${isHovered ? 'w-5/6' : 'w-full'} note-card font-bold`}>{title === '' ? 'New Note' : title}</p>
+        <p className={`${isHovered ? 'w-5/6' : 'w-full'} note-card`}>{content === '' ? '...' : content}</p>
       </button>
     </div>
   )
--- packages/frontend/src/components/NoteSelection.js
+++ packages/frontend/src/components/NoteSelection.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { add } from '../slices/noteDataSlice';
 import { NoteCard } from './NoteCard';
@@ -9,6 +9,9 @@ export const NoteSelection = () => {
     const selected = useSelector((state) => state.noteData.selectedNote);
     const user = useSelector((state) => state.auth.user);
     const dispatch = useDispatch();
+
+    const [hoverIndex, setHoverIndex] = useState(null);
+    const [hoverDelete, setHoverDelete] = useState(null);
 
     const handleNoteCreation = async () => {
         try {
@@ -34,11 +37,28 @@ export const NoteSelection = () => {
         }
     }
 
+    useEffect(() => {
+        if (notes.length === 0) {
+            setHoverIndex(null);
+            setHoverDelete(null);
+        }
+    }, [notes]);
+
     return (
         <div className='note-selection flex flex-col items-center gap-1 p-1 bg-black bg-opacity-20'>
-            {notes.map((note) => (
-                <NoteCard key={note.id} id={note.id} title={note.title} content={note.content} selected={note.id === selected.id}/>
-            ))}
+            {notes.map((note, index) => (
+                <NoteCard
+                    key={note.id}
+                    id={note.id}
+                    title={note.title}
+                    content={note.content}
+                    selected={note.id === selected.id}
+                    isHovered={index === hoverIndex}
+                    isHoveredDelete={index === hoverDelete}
+                    elementIndex={index}
+                    setHoverIndex={setHoverIndex}
+                    setHoverDelete={setHoverDelete}
+                />))}
             <button
                 className='border border-white text-white mx-auto my-3 px-3 py-1 hover:text-brightTeal hover:border-brightTeal'
                 onClick={handleNoteCreation}
