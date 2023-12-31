14391abe5914572e0bc6566470c486856774d717
Joe Pytlik <mail@joepytlik.de>
2023-12-31 01:56:22 +0100

    Updated tmux-dev.zsh

--- tmux-dev.zsh
+++ tmux-dev.zsh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 
+print_hashes() {
+  echo -n "# "
+  for i in {1..10}
+  do
+    echo -n "."
+    sleep 0.4
+  done
+}
+
 echo "> Starting Pencil Note App Development Environment"
 
 session="pencil-app"
@@ -9,16 +18,10 @@ tmux new-session -d -s $session
 tmux split-window -h -t $session:0
 tmux split-window -v -t $session:0
 
-echo -n "."
 tmux send-keys -t $session:0.0 'npm run backend' C-m
-sleep 0.5
-
-echo -n "."
 tmux send-keys -t $session:0.1 'npm run frontend' C-m
-sleep 0.5
-
-echo -n "."
 tmux send-keys -t $session:0.2 'npm run tailwind' C-m
-sleep 0.5
+
+print_hashes
 
 tmux attach -t $session
