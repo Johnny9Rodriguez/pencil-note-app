1b2de5ab2e796316d2794d1d50451728a9f54d50
Joe Pytlik <mail@joepytlik.de>
2024-09-30 23:01:52 +0200

    enable trust proxy

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -34,6 +34,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.enable('trust proxy');
 app.use(
     cors({
         origin: ['http://localhost:3000', 'https://pencil.joepytlik.de'],
