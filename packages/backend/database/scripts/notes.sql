a8514480fd321a5575535324eebfcf1e2641efec
Joe Pytlik <mail@joepytlik.de>
2024-09-30 00:48:57 +0000

    feat: add session sql file

--- packages/backend/database/scripts/notes.sql
+++ packages/backend/database/scripts/notes.sql
@@ -1,8 +1,9 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE notes (
-    user_id UUID PRIMARY KEY NOT NULL,
-    notes JSON,
-    last_updated BIGINT,
-    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
-);
+CREATE TABLE
+    notes (
+        user_id UUID PRIMARY KEY NOT NULL,
+        notes JSON,
+        last_updated BIGINT,
+        FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+    );
